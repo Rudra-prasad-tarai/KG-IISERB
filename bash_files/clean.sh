3bd473a1e37fda7305455919a3412bad2adf0e31
Rudra-Prasad-Tarai <rudratara8@gmail.com>
2025-11-17 15:13:37 +0530

    added cleand file and script for doing so

---
+++ bash_files/clean.sh
@@ -0,0 +1,7 @@
+python3 cleaner/clean_BS_MS.py --input ./html_dumps/ms_5th_year_page.html --output_dir ./cleaned_html
+python3 cleaner/clean_BS_MS.py --input ./html_dumps/bsms_4th_year_page.html --output_dir ./cleaned_html
+python3 cleaner/clean_BS_MS.py --input ./html_dumps/bsms_3rd_year_page.html --output_dir ./cleaned_html
+python3 cleaner/clean_rg.py --input html_dumps/researchGroups.html --output_dir ./cleaned_html
+python3 cleaner/clean_pd.py --input html_dumps/postdocs_page.html --output_dir ./cleaned_html
+python3 cleaner/clean_phds.py --input html_dumps/phd_iiserb_page.html --output_dir ./cleaned_html
+python3 cleaner/clean_faculty.py --input html_dumps/faculty_page.html --output_dir ./cleaned_html
